411cc8372f6c4a266f7150d3e9bd84b6483eb696
jgx <jules.gioux@gmail.com>
2020-07-02 16:26:16 +0200

    test

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
+<?php require_once "assets/controllers/index-controller.php"?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/level3xml.css">
+    <link rel="stylesheet" href="<?=$css?>">
+    <title>Accueil</title>
 </head>
 <body>
     
+   
 </body>
 </html>
