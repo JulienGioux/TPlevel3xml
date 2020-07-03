bbbe291c7d069dff79db91d690399e067012e576
jgx <jules.gioux@gmail.com>
2020-07-03 15:18:05 +0200

    COOKIES OK !!

--- controllers/index-controller.php
+++ controllers/index-controller.php
@@ -13,7 +13,6 @@ else {
     $testCookie = false;
 }
 
-var_dump($_POST);
 //initialisation
 setlocale(LC_TIME, 'french.UTF-8, fr-FR.UTF-8', 'fr.UTF-8', 'fra.UTF-8', 'fr_FR.UTF-8');
 date_default_timezone_set('Europe/Paris');
@@ -23,17 +22,7 @@ $urlApps = "https://www.01net.com/rss/actualites/applis-logiciels/";
 $urlTech = "https://www.01net.com/rss/actualites/technos/";
 $urlBuzz = "https://www.01net.com/rss/actualites/buzz-societe/";
 
-$rssDefChoice = [$urlActu, $urlSecu, $urlApps];
 
-$rssActu = simplexml_load_file($urlActu);
-
-$item = $rssActu->channel->item[1];
-$title = $item->title;
-$desc = $item->description;
-$link = $item->link;
-$date = $item->pubDate;
-$img = $item->enclosure;
-$rssChoice = [$urlActu, $urlSecu, $urlApps];
 $css = 'assets/css/blackTheme.css';
 
 //Traite les données de formulaire, besoin de vérifs supplémentaires
@@ -66,17 +55,16 @@ if (isset($_POST) && !empty($_POST)) {
         $rssChoice = [];
         foreach ($_POST['subCheck'] as $key => $value) {            
             $rssChoice[$key] = $value;
-        }        
-     } else {
-        $rssChoice = [$urlActu, $urlSecu, $urlApps];
-     } 
+        }
+        setcookie("rssChoice", json_encode($rssChoice), time()+31556926 ,'/');       
+     }
 }
 if (isset($_POST) && !empty($_POST) && $testCookie) {
     header("Location: $_SERVER[PHP_SELF]");
 }
 
 $articlesNumber = $_POST['articlesNumber'] ?? $_COOKIE['articlesNumber'] ?? 3;
-
+$rssChoice = $rssChoice ?? json_decode($_COOKIE['rssChoice']) ?? [$urlActu, $urlSecu, $urlApps];
 //renvoie les infos d'un élément d'article article en fonction du flux, de son index
 function sortItem($rss,$i,$el) {
     $item = $rss->channel->item[$i];
