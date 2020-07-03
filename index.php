a4a8c997eee1f79a45ab2aa9a687b77ece3df188
Thibaud <thipicot@gmail.com>
2020-07-03 12:11:46 +0200

    ajustement balise html form parametres

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
         <?php }
         } ?>
 
-        
+
 
 
     </main>
@@ -67,93 +67,92 @@
         <!-- mentions légales -->
     </footer>
 
-        <!-- Modal -->
-        <div class="modal fade" id="parametersModal" tabindex="-1" role="dialog" aria-labelledby="parametersModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Paramètres</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <form id="params" action="index.php" method="post">
-                        <div class="modal-body">
-                            
-                                <div class="row">
-                                    <div class="col">
-                                        <label for="themeColor">Thème</label>
-                                        <select class="form-control" id="colorTheme" name="colorTheme">
-                                            <option value="black">Noir</option>
-                                            <option value="blue">Bleu</option>
-                                            <option value="red">Rouge</option>
-                                        </select>
-                                    </div>
-                                    <div class="col">
-                                        <label for="articlesNumber">Nombre d'articles</label>
-                                        <select class="form-control" id="artcilesNumber" name="articlesNumber">
-                                            <option value="3">3</option>
-                                            <option value="5">5</option>
-                                            <option value="8">8</option>>
-                                        </select>
-                                    </div>
-                                    <div class="col">
-                                        <legend class="col-form-label col">Sujets</legend>
-                                        <div>
-                                            <input type="checkbox" id="actuCheck" name="subCheck[]" value="<?=$urlActu?>" checked="true">
-                                            <label for="actuCheck">Actualités</label><br>
-                                            <input type="checkbox" id="secuCheck" name="subCheck[]" value="<?=$urlSecu?>" checked="true">
-                                            <label for="secuCheck"> Sécurité</label><br>
-                                            <input type="checkbox" id="appliCheck" name="subCheck[]" value="<?=$urlApps?>" checked="true">
-                                            <label for="appliCheck"> Applications</label><br>
-                                            <input type="checkbox" id="technoCheck" name="subCheck[]" value="<?=$urlTech?>">
-                                            <label for="technoCheck">Technologie</label><br>
-                                            <input type="checkbox" id="buzzCheck" name="subCheck[]" value="<?=$urlBuzz?>">
-                                            <label for="buzzCheck">Buzz</label><br>
-                                        </div>
-                                    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="parametersModal" tabindex="-1" role="dialog" aria-labelledby="parametersModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Paramètres</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form id="params" action="index.php" method="post">
+                    <div class="modal-body">
+
+                        <div class="row">
+                            <div class="col">
+                                <label for="themeColor">Thème</label>
+                                <select class="form-control" id="colorTheme" name="colorTheme">
+                                    <option value="black">Noir</option>
+                                    <option value="blue">Bleu</option>
+                                    <option value="red">Rouge</option>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <label for="articlesNumber">Nombre d'articles</label>
+                                <select class="form-control" id="artcilesNumber" name="articlesNumber">
+                                    <option value="3">3</option>
+                                    <option value="5">5</option>
+                                    <option value="8">8</option>>
+                                </select>
+                            </div>
+                            <div class="col">
+                                <legend class="col-form-label col">Sujets</legend>
+                                <div>
+                                    <input type="checkbox" id="actuCheck" name="subCheck[]" value="<?= $urlActu ?>" checked="true">
+                                    <label for="actuCheck">Actualités</label><br>
+                                    <input type="checkbox" id="secuCheck" name="subCheck[]" value="<?= $urlSecu ?>" checked="true">
+                                    <label for="secuCheck"> Sécurité</label><br>
+                                    <input type="checkbox" id="appliCheck" name="subCheck[]" value="<?= $urlApps ?>" checked="true">
+                                    <label for="appliCheck"> Applications</label><br>
+                                    <input type="checkbox" id="technoCheck" name="subCheck[]" value="<?= $urlTech ?>">
+                                    <label for="technoCheck">Technologie</label><br>
+                                    <input type="checkbox" id="buzzCheck" name="subCheck[]" value="<?= $urlBuzz ?>">
+                                    <label for="buzzCheck">Buzz</label><br>
                                 </div>
                             </div>
-
                         </div>
-                        <div class="modal-footer">
-                            <button type="reset" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                            <button type="submit" class="btn btn-warning" name="submit">Sauvegarder</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-        
-         <!-- modal article -->
-         <div class="modal fade" id="articlesModal" tabindex="-1" role="dialog" aria-labelledby="articlesModal" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header bg-warning">
-                        <div class="modal-title text-white h6" id="exampleModalLabel"><?= $date ?></div>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
                     </div>
-                    <form id="params" action="index.php" method="post">
-                        <div class="modal-body mt-2">
-                            
-                                <div class="row text-center h5 mb-3 font-weith-bold"><?= $title ?></div>
-                                <div class="row h6 m-1"><?= $desc ?></div>
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="reset" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                            <button type="submit" class="btn btn-warning" name="submit">Aller vers l'article</button>
-                        </div>
-                    </form>
-                </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-warning" name="submit">Sauvegarder</button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
+
+    <!-- modal article -->
+    <div class="modal fade" id="articlesModal" tabindex="-1" role="dialog" aria-labelledby="articlesModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header bg-warning">
+                    <div class="modal-title text-white h6" id="exampleModalLabel"><?= $date ?></div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form id="params" action="index.php" method="post">
+                    <div class="modal-body mt-2">
+
+                        <div class="row text-center h5 mb-3 font-weith-bold"><?= $title ?></div>
+                        <div class="row h6 m-1"><?= $desc ?></div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="reset" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        <button type="submit" class="btn btn-warning" name="submit">Aller vers l'article</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <script src="assets/js/testCheckBox.js"></script>
+
 </html>
