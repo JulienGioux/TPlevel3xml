e703b64ffa239c8adc39a3d6e1524e57c6ec7f67
jgx <jules.gioux@gmail.com>
2020-07-05 00:49:50 +0200

    v1.0.2b

--- pages/header.php
+++ pages/header.php
@@ -1,4 +1,6 @@
 <header class="d-flex align-items-center justify-content-center">
-    <p class="h1">SUPER RSS READER</p>
-    <div><img src="assets\img\rss-logo-icon-png-11302.png" alt="icon_fluxrss" class="iconFlux"></div>
+    <a href="index.php">
+        <p class="h1">SUPER RSS READER</p>
+    </a>
+    <div><img src="assets\img\rss-logo-icon-png-11302.png" alt="icon_fluxrss" class="iconFlux"></div>    
 </header>
