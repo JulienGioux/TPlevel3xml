3a425b4e912f790610f970f4b0140af47a90ba5d
Sulian-Gouard <suliangouardlanoe@gmail.com>
2020-07-06 14:32:16 +0200

    favicon

--- pages/head.php
+++ pages/head.php
@@ -4,6 +4,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/level3xml.css">
     <link rel="stylesheet" href="<?= $cssTheme ?>">
+    <link rel="icon" type="image/png" href="assets\img\favicon.svg" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
     <title>Super RSS</title>
 </head>
